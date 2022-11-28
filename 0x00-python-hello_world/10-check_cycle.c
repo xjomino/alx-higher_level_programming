095b7c60a610c53137361d1c3424df1f47ce5fdb
xjomino <jammwa@gmail.com>
2022-11-28 19:12:56 +0000

    correction

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,28 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * check_cycle - count the cycle
- * @list: input
- * Return: 0 if ok and 1 if not
+ * check_cycle - list
+ * @head:  type list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
  */
-
-int check_cycle(listint_t *list)
+int check_cycle(listint_t *head)
 {
+	int *node1, *node2;
 
-	listint_t *current;
-
-	if (list)
-	{
-		while (list != NULL)
-		{
-			current = list;
-			list = list->next;
-			if (current <= list)
-				return (1);
-		}
+	if (head == NULL)
 		return (0);
+
+	while (head != NULL)
+	{
+		node1 = (int *)&head;
+		node2 = (int *)&head->next;
+		if (head->next == NULL)
+			return (0);
+
+		if (*node1 - *node2 <= 0)
+			return (1);
+
+		head = head->next;
 	}
 	return (0);
 }
