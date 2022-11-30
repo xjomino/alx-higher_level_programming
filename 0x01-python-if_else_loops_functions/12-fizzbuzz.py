46811aa742a00a79d757e29aefab5705c41337c6
xjomino <jammwa@gmail.com>
2022-11-30 01:16:24 +0000

    modified:   12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for k in range(1, 101):
         if (k % 15 == 0):
-            print("FizzBuzz ", end="")
+            print("FizzBuzz ", end=" ")
         elif (k % 3 == 0):
             print("Fizz ", end=" ")
         elif (k % 5 == 0):
