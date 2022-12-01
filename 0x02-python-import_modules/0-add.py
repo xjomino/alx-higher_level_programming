ca7195b8ad9f12a9dcd6a218d71d1625c13a052a
xjomino <jammwa@gmail.com>
2022-12-01 12:20:39 +0000

    corrections

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0 as jm
-a = 1
-b = 2
-addition = jm.add(a, b)
-print("{} + {} = {}".format(a, b, addition))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
