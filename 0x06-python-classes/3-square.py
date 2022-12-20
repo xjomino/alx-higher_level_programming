4ed4cf818dc6a52894cd8eb09c646f1428034d8c
xjomino <jammwa@gmail.com>
2022-12-20 09:49:10 +0000

    classes

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._size = size
+        def area(self):
+            return self._size ** 2
