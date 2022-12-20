8bd2011d2c4ca45b56e846c4cc848c9021eb5d05
xjomino <jammwa@gmail.com>
2022-12-20 15:23:51 +0000

    corrections

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""define class Square"""
+
+
 class Square:
+    """define Square"""
+
     def __init__(self, size=0):
-        if not isinstance(size, int):
+        """initializer object"""
+
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
