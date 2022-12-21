c3b5f56e22dd1718359b5bd72317d93281e830c1
xjomino <jammwa@gmail.com>
2022-12-21 01:15:03 +0000

    corrections

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -10,6 +10,10 @@ class Square:
 
         self.__size = size
 
+    def area(self):
+        """return area of the Square in the atribute private __size"""
+
+        return self.__size ** 2
 
     @property
     def size(self):
@@ -23,8 +27,3 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
-    def area(self):
-        """return area of the Square in the atribute private __size"""
-
-        return self.__size ** 2
