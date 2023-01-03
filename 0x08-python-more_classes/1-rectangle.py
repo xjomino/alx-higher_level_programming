2159b67e8771cc807e971d43748de9999338a971
xjomino <jammwa@gmail.com>
2023-01-03 15:30:53 +0000

    correcttions

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,29 +1,41 @@
 #!/usr/bin/python3
+"""
+Module contains asingle class
+"""
+
+
 class Rectangle:
+    """Defines a Rectangle"""
+
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        """Initilising of the instance object"""
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
-        return self._width
+        """Retrieves the value of value `width`"""
+        return self.__width
 
     @width.setter
     def width(self, value):
+        """Sets `width` value to new value"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
-        return self._height
+        """Retrieves the value of value `height`"""
+        return self.__height
 
     @height.setter
     def height(self, value):
+        """Sets `height` value to new value"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
+        self.__height = value
