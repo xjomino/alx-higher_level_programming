6291576540f9f4baf49255b394a148449dcb9c2d
xjomino <jammwa@gmail.com>
2023-01-09 17:47:18 +0000

    modified:   1-my_list.py 	new file:   2-is_same_class.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def greet(self):
-        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
-        p = Person("John", 30)
-        attributes_and_methods = lookup(p)
-        print(attributes_and_methods)
-
+class MyList(list):
+    def print_sorted(self):
+        """Create a copy of the list and sort it in ascending orderi"""
+        sorted_list = self.copy()
+        sorted_list.sort()
+        print(sorted_list)
