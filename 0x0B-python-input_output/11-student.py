8ecb94aa0b69c03c9ddf0a1c30a635895dbb7004
xjomino <jammwa@gmail.com>
2023-01-10 16:31:53 +0000

    test

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Student function documentation"""
+
+
+class Student:
+    """defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """instance initializer"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves dictionart"""
+
+        dict = vars(self)
+        if attrs is None:
+            return dict
+
+        j = {f: m for f, m in dict.items() if f in attrs}
+        return j
+
+    def reload_from_json(self, json):
+        """
+        replaces all attributes into student instance
+        """
+        dict.update(json)
