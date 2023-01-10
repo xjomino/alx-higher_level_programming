bf16c39457e78f12a5ac2b2c4d53c834203add3a
xjomino <jammwa@gmail.com>
2023-01-10 12:20:50 +0000

    class to json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-def class_to_json(obj):
-    # Use the built-in function vars to get the object's attributes
-    return vars(obj)
+"""serialize an object"""
 
+
+def class_to_json(obj):
+    """Use the built-in function vars to
+    get the object's attributes"""
+
+    return vars(obj)
