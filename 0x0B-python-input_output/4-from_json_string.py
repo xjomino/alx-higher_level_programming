d66f032408a69b47eaf31b2bdedf06a3261c6509
xjomino <jammwa@gmail.com>
2023-01-10 11:55:51 +0000

    corrections

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""JSON handling"""
 import json
 
+
 def from_json_string(my_str):
+    """ function that returns an object
+    (Python data structure) represented
+    by a JSON string
+    """
+
     return json.loads(my_str)
