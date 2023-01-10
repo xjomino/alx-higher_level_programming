f3f164417b53a79ebaccfda47caa26ae0f147cfe
xjomino <jammwa@gmail.com>
2023-01-10 12:25:10 +0000

    student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
+"""Student function documentation"""
+
+
 class Student:
+    """defines a student"""
+
     def __init__(self, first_name, last_name, age):
+        """instance initializer"""
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-def to_json(self):
-    return vars(self)
+
+    def to_json(self):
+        """retrieves dictionart"""
+
+        return vars(self)
