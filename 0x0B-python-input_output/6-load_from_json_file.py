b40b280e5eca0e8007bd93386ac0fe04840d0bd0
xjomino <jammwa@gmail.com>
2023-01-10 12:07:56 +0000

    corrections

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""JSON handling"""
 import json
 
+
 def load_from_json_file(filename):
-    with open(filename, "r") as file:
+    """function that creates an Object
+    from a “JSON file”
+    """
+
+    with open(filename, mode="r") as file:
         return json.load(file)
