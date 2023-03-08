a2ea2f3af0064eb79a8f530bb943e79be2288514
xjomino <jammwa@gmail.com>
2023-03-08 07:38:42 +0000

    corrections

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
-i-- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL
+-- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL
 -- database name will be passed as an argument of the mysql command
 SELECT score, name FROM second_table ORDER BY score DESC;
