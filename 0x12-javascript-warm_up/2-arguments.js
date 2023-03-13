b82b610b2cc7fe20b15c4b0ffa04ad454b8fb167
xjomino <jammwa@gmail.com>
2023-03-13 19:02:57 +0000

    java is fun

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const argsCount = process.argv.length - 2;
+const entry = process.argv.length;
 
-if (argsCount === 0) {
-  console.log("No argument");
-} else if (argsCount === 1) {
-  console.log("Argument found");
+if (entry < 3) {
+  console.log('No argument');
+} else if (entry === 3) {
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
